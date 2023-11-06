dce8b9013e4c60c5753a9776ee50c6624a6bfc3d
Priv-u <priv-u@mail.ru>
2023-11-06 12:43:56 +0600

    Начало работы с новым приложением на C#

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace App2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Ни как не удается настроить запуск приложения в консоли VSCode");
+            Console.WriteLine("Вот теперь всё получилось. Причина была в том, что нужно переменную окружения добавить путь .Net SDK");
+            Console.ReadKey();
+        }
+    }
+}
